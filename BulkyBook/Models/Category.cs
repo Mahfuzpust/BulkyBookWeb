4cff9b34c93bdc3a2da6fb2d0d5f8456f05a366c
mahfuz khan <mahfuzpust@gmail.com>
2023-01-14 23:09:15 +0600

    first commit

---
+++ BulkyBook/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+        [Range(0, 100, ErrorMessage ="The number between 1 to 100 !!")]
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+    }
+}
